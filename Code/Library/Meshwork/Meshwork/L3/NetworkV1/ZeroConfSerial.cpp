5dbf21af0445ca57c80bf3942df39fb8a962971c
Sinisha Djukic <s.djukic@prosyst.com>
2014-10-18 16:36:02 +0300

    Added CfgSerial message support
    Using early-initialized config object instead of mesh object

--- Code/Library/Meshwork/Meshwork/L3/NetworkV1/ZeroConfSerial.cpp
+++ Code/Library/Meshwork/Meshwork/L3/NetworkV1/ZeroConfSerial.cpp
@@ -93,7 +93,7 @@ bool Meshwork::L3::NetworkV1::ZeroConfSerial::processZCID(serialmsg_t* msg) {
 
 bool Meshwork::L3::NetworkV1::ZeroConfSerial::processZCNwkID(serialmsg_t* msg) {
 	MW_LOG_INFO(LOG_ZEROCONFSERIAL, "SERSEQ=%d, Channel=%d, NwkID=%d, NodeID=%d", msg->seq, m_nwkconfig->channel, m_nwkconfig->nwkid, m_nwkconfig->nodeid);
-	uint8_t data[] = {m_currentMsg->seq, 5, MSGCODE_ZCNWKIDRES, m_network->getChannel(), ( m_nwkconfig->nwkid >> 8 ) && 0xFF, m_nwkconfig->nwkid && 0xFF, m_network->getNodeID()};
+	uint8_t data[] = {m_currentMsg->seq, 5, MSGCODE_ZCNWKIDRES, m_nwkconfig->channel, ( m_nwkconfig->nwkid >> 8 ) && 0xFF, m_nwkconfig->nwkid && 0xFF, m_nwkconfig->nodeid};
 	writeMessage(sizeof(data), data, true);
 	return true;
 }
@@ -140,6 +140,47 @@ bool Meshwork::L3::NetworkV1::ZeroConfSerial::processZCCfgNwk(serialmsg_t* msg) 
 	return result;
 }
 
+bool Meshwork::L3::NetworkV1::ZeroConfSerial::processZCCfgSerial(serialmsg_t* msg) {
+	if ( !m_initmode ) {
+		MW_LOG_ERROR(LOG_ZEROCONFSERIAL, "Must be in ZCInit mode!", NULL);
+		respondNOK(msg, ERROR_ILLEGAL_STATE);
+		return false;
+	}
+#ifndef ZEROCONF_SERIAL_CHANGE_ENABLE
+	if ( m_sernum->sernumlen > 0 ) {
+		MW_LOG_ERROR(LOG_ZEROCONFSERIAL, "Serial number already written and cannot be modified!", NULL);
+		respondNOK(msg, ERROR_ILLEGAL_STATE);
+		return false;
+	}
+#endif
+
+	bool result = false;
+	if ( m_serial->available() >= 1 ) {
+		m_sernum->sernumlen = m_serial->getchar();
+		m_sernum->sernum[0] = 0;
+		
+		
+		if ( m_sernum->sernumlen <= MAX_SERIAL_LEN ) {
+			for (size_t i = 0; i < m_sernum->sernumlen; i ++ )
+				m_sernum->sernum[i] = m_serial->getchar();
+			if ( m_sernum->sernumlen > 0 )
+				m_sernum->sernum[m_sernum->sernumlen + 1] = 0;//zero-terminate
+			result = true;
+		} else {
+			m_sernum->sernumlen = 0;
+		}
+		MW_LOG_INFO(LOG_ZEROCONFSERIAL, "SerNumLen=%d, SerNum=%s", m_sernum->sernumlen, m_sernum->sernum);
+		if ( m_listener != NULL )
+			m_listener->serial_updated();
+		//the only possible fail point, so far
+		respondWCode(msg, result ? MSGCODE_OK : ERROR_SERIAL_TOO_LONG);
+	} else {
+		MW_LOG_ERROR(LOG_ZEROCONFSERIAL, "Not enough data in ZCCfgSerial, ERROR_INSUFFICIENT_DATA", NULL);
+		respondNOK(msg, ERROR_INSUFFICIENT_DATA);
+	}
+	return result;
+}
+
 bool Meshwork::L3::NetworkV1::ZeroConfSerial::processZCCfgRep(serialmsg_t* msg) {
 	if ( !m_initmode ) {
 		MW_LOG_ERROR(LOG_ZEROCONFSERIAL, "Must be in ZCInit mode!", NULL);
@@ -163,7 +204,6 @@ bool Meshwork::L3::NetworkV1::ZeroConfSerial::processZCCfgRep(serialmsg_t* msg) 
 	}
 	return result;
 }
-
 
 bool Meshwork::L3::NetworkV1::ZeroConfSerial::processOneMessageEx(serialmsg_t* msg) {
 	UNUSED(msg);
@@ -190,6 +230,7 @@ bool Meshwork::L3::NetworkV1::ZeroConfSerial::processOneMessage(serialmsg_t* msg
 					case MSGCODE_ZCNWKID: processZCNwkID(msg); break;
 					case MSGCODE_ZCCFGNWK: processZCCfgNwk(msg); break;
 					case MSGCODE_ZCCFGREP: processZCCfgRep(msg); break;
+					case MSGCODE_ZCCFGSERIAL: processZCCfgSerial(msg); break;
 					default:
 						result = processOneMessageEx(msg);
 						if (!result)
