788d05cdada2bd39cc5c08fc145af36c592d60c6
Sinisha Djukic <s.djukic@prosyst.com>
2014-10-18 16:33:02 +0300

    Added L3 ZerConf batch file

---
+++ Code/Host/JMeshwork/org.meshwork.assembly/src/main/L3ZeroConf.bat
@@ -0,0 +1,11 @@
+@echo off
+echo ..............
+java -version
+echo ..............
+set CMDLINE=java -Xmx256M -classpath lib/org.meshwork.core.api.jar;lib/org.meshwork.core.host.l3.jar;lib/org.meshwork.core.transport.serial.jssc.jar;lib/org.meshwork.core.zeroconf.l3.jar;lib/org.meshwork.app.zeroconf.l3.node.jar;lib/jssc.jar org.meshwork.app.zeroconf.l3.node.serial.ConsoleRouterImpl %1 %2 %3 %4 %5
+
+echo %CMDLINE%
+
+%CMDLINE%
+
+pause
