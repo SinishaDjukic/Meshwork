1d8da22b0924dd8cccc7bef538f3ab94f4a0fa03
Sinisha Djukic <s.djukic@prosyst.com>
2015-01-02 00:12:08 +0200

    Packages refactored

--- Code/Host/JMeshwork/org.meshwork.assembly/src/main/L3PerfTestArrayConsole.bat
+++ Code/Host/JMeshwork/org.meshwork.assembly/src/main/L3PerfTestArrayConsole.bat
@@ -2,7 +2,7 @@
 echo ..............
 java -version
 echo ..............
-set CMDLINE=java -Xmx256M -classpath lib/org.meshwork.core.api.jar;lib/org.meshwork.core.host.l3.jar;lib/org.meshwork.core.transport.serial.jssc.jar;lib/org.meshwork.app.host.l3.router.jar;lib/jssc.jar org.meshwork.app.host.l3.router.serial.ConsolePerformanceTestArrayImpl %*
+set CMDLINE=java -Xmx256M -classpath lib/org.meshwork.core.api.jar;lib/org.meshwork.core.host.l3.jar;lib/org.meshwork.core.transport.serial.jssc.jar;lib/org.meshwork.app.host.l3.router.jar;lib/jssc.jar org.meshwork.app.host.l3.router.console.ConsolePerformanceTestArrayImpl %*
 
 echo %CMDLINE%
 
--- Code/Host/JMeshwork/org.meshwork.assembly/src/main/L3ZeroConf.bat
+++ Code/Host/JMeshwork/org.meshwork.assembly/src/main/L3ZeroConf.bat
@@ -2,7 +2,7 @@
 echo ..............
 java -version
 echo ..............
-set CMDLINE=java -Xmx256M -classpath lib/org.meshwork.core.api.jar;lib/org.meshwork.core.host.l3.jar;lib/org.meshwork.core.transport.serial.jssc.jar;lib/org.meshwork.core.zeroconf.l3.jar;lib/org.meshwork.app.zeroconf.l3.node.jar;lib/jssc.jar org.meshwork.app.zeroconf.l3.node.serial.ConsoleRouterImpl %1 %2 %3 %4 %5
+set CMDLINE=java -Xmx256M -classpath lib/org.meshwork.core.api.jar;lib/org.meshwork.core.host.l3.jar;lib/org.meshwork.core.transport.serial.jssc.jar;lib/org.meshwork.core.zeroconf.l3.jar;lib/org.meshwork.app.zeroconf.l3.node.jar;lib/jssc.jar org.meshwork.app.zeroconf.l3.node.console.ConsoleNodeImpl %1 %2 %3 %4 %5
 
 echo %CMDLINE%
 
